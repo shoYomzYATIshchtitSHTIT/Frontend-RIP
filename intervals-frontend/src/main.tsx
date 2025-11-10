76d2ea366776a56d46c8ae0c3e19f1c58b829868
shoYomzYATIshchtitSHTIT <afdf52@mail.ru>
2025-11-10 21:41:24 +0300

    add pwa

--- intervals-frontend/src/main.tsx
+++ intervals-frontend/src/main.tsx
@@ -4,13 +4,18 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App.tsx'
+import { registerSW } from "virtual:pwa-register"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+
+if ("serviceWorker" in navigator) {
+    registerSW()
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter> {/* БЕЗ basename */}
                 <App />
             </BrowserRouter>
         </Provider>
--- intervals-frontend/vite.config.ts
+++ intervals-frontend/vite.config.ts
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "База музыкальных интервалов",
+        short_name: "Интервалы",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#7978F7",
+        theme_color: "#7978F7",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/img/logo192.png",  // путь от корня сайта
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "/img/logo512.png",
+            type: "image/png",
+            sizes: "512x512"
+          }
+        ],
+      },
+    })
+  ],
+  // base: "/Frontend-RIP/", // НЕ ИСПОЛЬЗУЕМ для разработки
   server: {
     port: 3000,
     proxy: {
